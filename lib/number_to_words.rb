6abbed8641f2d4e745486c00dbc38c6296400915
Annie Shin, Rita Bennett-Chew and Reese Glasscock <rita@>
2018-06-26 10:52:20 -0700

    test 3 fails

--- lib/number_to_words.rb
+++ lib/number_to_words.rb
@@ -6,7 +6,9 @@ class NumberToWords
 
   def translate()
     ones = {0 => "zero", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine"}
+    teens = {11 => 'eleven', 12 => 'twelve', 13 => 'thirteen', 14 => 'fourteen', 15 => 'fifteen', 16 => 'sixteen', 17 => 'seventeen', 18 => 'eighteen', 19=> 'nineteen'}
     ones.fetch(@number)
+    teens.fetch(@number)
   end
 
 end
