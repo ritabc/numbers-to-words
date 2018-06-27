729fdd039c3278784f054a412162ee38e68dd511
Annie Shin, Rita Bennett-Chew and Reese Glasscock <rita@>
2018-06-26 18:45:12 -0700

    remove initial spec and rb lib file

--- spec/number_to_words_spec.rb
+++ spec/number_to_words_spec.rb
@@ -1,46 +1,49 @@
-# # require 'rspec'
-# # require 'number_to_words'
-# require 'pry'
-#
-# describe ("NumberToWords#translate") do
-#   it("will take the number 3 and return 'three'") do
-#     number = NumberToWords.new(3)
-#     expect(number.translate()).to(eq("three"))
-#   end
-#   it("will take the number 7 and return 'seven'") do
-#     number = NumberToWords.new(7)
-#     expect(number.translate()).to(eq("seven"))
-#   end
-#   it "will take the number 11 and return eleven" do
-#     number = NumberToWords.new(11)
-#     expect(number.translate()).to(eq("eleven"))
-#   end
-#   it "will take the number 10 and return ten" do
-#     number = NumberToWords.new(10)
-#     expect(number.translate()).to(eq("ten"))
-#   end
-#   it "will take the number 23 and return twenty two" do
-#     number = NumberToWords.new(23)
-#     expect(number.translate()).to(eq("twenty three"))
-#   end
-#   it "will take the number 100 and return one hundred" do
-#     number = NumberToWords.new(200)
-#     expect(number.translate()).to(eq("two hundred"))
-#   end
-#   it "will take the number 119 and return one hundred nineteen" do
-#     number = NumberToWords.new(217)
-#     expect(number.translate()).to(eq("two hundred seventeen"))
-#   end
-#   it "will take the number 123 and return one hundred twenty three" do
-#     number = NumberToWords.new(123)
-#     expect(number.translate()).to(eq("one hundred twenty three"))
-#   end
-#   it "will take the number 1000 and return one thousand" do
-#     number = NumberToWords.new(1000)
-#     expect(number.translate()).to(eq("one thousand"))
-#   end
-#   it "will take the number 240 and two hundred forty" do
-#     number = NumberToWords.new(240)
-#     expect(number.translate()).to(eq("two hundred forty"))
-#   end
-# end
+require 'rspec'
+require 'number_to_words'
+require 'pry'
+
+describe ("NumberToWords#translate") do
+  it "will take the number 2 and return the word two" do
+    number = NumberToWords.new(2)
+    expect(number.translate()).to(eq('two'))
+  end
+  it "will take the number 11 and return eleven" do
+    number = NumberToWords.new(11)
+    expect(number.translate()).to(eq("eleven"))
+  end
+  it "will take the number 21 and return twenty one" do
+    number = NumberToWords.new(21)
+    expect(number.translate()).to(eq("twenty one"))
+  end
+  it "will take the number 97 and return ninety seven" do
+    number = NumberToWords.new(97)
+    expect(number.translate()).to(eq("ninety seven"))
+  end
+  it "will take the number 100 and return one hundred" do
+    number = NumberToWords.new(123)
+    expect(number.translate()).to(eq("one hundred twenty three"))
+  end
+  it "will take the number 100 and return one hundred" do
+    number = NumberToWords.new(100)
+    expect(number.translate()).to(eq("one hundred"))
+  end
+
+  it "will take the number 867 and return eight hundred sixty seven" do
+    number = NumberToWords.new(867)
+    expect(number.translate()).to(eq("eight hundred sixty seven"))
+  end
+
+  it "will take the number 4687 and return four thousand six hundred eighty seven" do
+    number = NumberToWords.new(4687)
+    expect(number.translate()).to(eq("four thousand six hundred eighty seven"))
+  end
+
+  it "will take the number 20,687 and return twenty thousand six hundred eighty seven" do
+    number = NumberToWords.new(20687)
+    expect(number.translate()).to(eq("twenty thousand six hundred eighty seven"))
+  end
+  it "will take the number 14,687 and return twenty thousand six hundred eighty seven" do
+    number = NumberToWords.new(99687)
+    expect(number.translate()).to(eq("ninety nine thousand six hundred eighty seven"))
+  end
+end
